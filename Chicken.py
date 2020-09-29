206d24cc981b94107192ad9b7ff0c4f928d19aab
LuckyDDBH <60917423+LuckyDDBH@users.noreply.github.com>
2020-09-29 14:46:51 +0200

    Add files via upload

--- Chicken.py
+++ Chicken.py
@@ -15,10 +15,13 @@ class Chicken:
         self.image = pygame.image.load("Choiken.gif")
         self.bigImage = pygame.transform.scale(self.image, (55, 55))
 
-    stepCounter = 0
+    stepCounter: int = 0
 
-    gridPlacementX = 0
-    gridPlacementY = 0
+    gridPlacementX: int = 0
+    gridPlacementY: int = 0
+
+    isTileCorrect: bool = False
+    whichTile: int
 
     def drawChicken(self, posX ,posY):
         self.displayName.blit(self.bigImage, (posX, posY))
@@ -27,28 +30,75 @@ class Chicken:
         self.stepCounter += 1
 
         if   self.stepCounter < 100:
-             self.gridPlacementX = 0
+            self.gridPlacementX = 0
 
         elif self.stepCounter > 100 and self.stepCounter < 200:
-             self.gridPlacementX = 1
+            self.gridPlacementX = 1
 
         elif self.stepCounter > 200 and self.stepCounter < 300:
-             self.gridPlacementX = 2
+            self.gridPlacementX = 2
 
         elif self.stepCounter == 300:
-             self.gridPlacementX = 0
-             self.stepCounter   = 0
-             self.gridPlacementY += 1
+            self.gridPlacementX = 0
+            self.stepCounter = 0
+            if self.gridPlacementY == 2:
+                self.gridPlacementY = 0
+            else:
+                self.gridPlacementY += 1
 
-             if self.gridPlacementY == 3:
-                 self.gridPlacementY = 0
 
     def eatGrass(self):
-        if self.gridPlacementX == 0 and self.gridPlacementY == 0:
-            if self.main.farmland[0].grid[0][0].isGrown:
-                self.main.farmland[0].grid[0][0].animating = True
-                self.main.farmland[0].grid[0][0].isGrown = False
-                self.main.coins += 2
+        for i in range(3):
+            for j in range(3):
+                if self.gridPlacementX == i and self.gridPlacementY == j:
+                    if self.main.farmland[0].grid[i][j].isGrown:
+                        self.main.farmland[0].grid[i][j].animating = True
+                        self.main.farmland[0].grid[i][j].isGrown = False
+                        self.main.coins += 2
+
+    def checkTile(self):
+        for i in range(3):
+            for j in range(3):
+                if self.gridPlacementX == i and self.gridPlacementY == j:
+                    self.isTileCorrect = True
+
+                    if   j == 0 and i == 0 and self.isTileCorrect == True:
+                        self.whichTile = 1
+
+                    elif j == 0 and i == 1 and self.isTileCorrect == True:
+                        self.whichTile = 2
+
+                    elif j == 0 and i == 2 and self.isTileCorrect == True:
+                        self.whichTile = 3
+
+                    elif j == 1 and i == 0 and self.isTileCorrect == True:
+                        self.whichTile = 4
+
+                    elif j == 1 and i == 1 and self.isTileCorrect == True:
+                        self.whichTile = 5
+
+                    elif j == 1 and i == 2 and self.isTileCorrect == True:
+                        self.whichTile = 6
+
+                    elif j == 2 and i == 0 and self.isTileCorrect == True:
+                        self.whichTile = 7
+
+                    elif j == 2 and i == 1 and self.isTileCorrect == True:
+                        self.whichTile = 8
+
+                    elif j == 2 and i == 2 and self.isTileCorrect == True:
+                        self.whichTile = 9
+
+    #def checkUnlock(self):
+        #self.main.farmland[0].grid[0][0].islocked
+        #if self.main.farmland[0].grid[0][0].islocked
+
+
+
+
+
+
+
 
 
 
