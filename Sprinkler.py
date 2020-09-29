ea7ae280e4d434b538f30a7d25258cad59fea32f
Johan L. Jensen <johanljensen@hotmail.com>
2020-09-29 12:29:59 +0200

    Sprinkler index fix

--- Sprinkler.py
+++ Sprinkler.py
@@ -23,21 +23,21 @@ class Sprinkler(FarmGadget):
             self.neighbouringTiles.append(self.farmBoard.grid[tileX -1, tileY -1])
         if(tileX > 0):
             self.neighbouringTiles.append(self.farmBoard.grid[tileX -1, tileY])
-        if(tileX > 0 and tileY < boardY):
+        if(tileX > 0 and tileY < boardY-1):
             self.neighbouringTiles.append(self.farmBoard.grid[tileX -1, tileY +1])
 
         #Set tiles above and below
         if(tileY > 0):
             self.neighbouringTiles.append(self.farmBoard.grid[tileX, tileY -1])
-        if(tileY < boardY):
+        if(tileY < boardY-1):
             self.neighbouringTiles.append(self.farmBoard.grid[tileX, tileY +1])
 
         #Set 3 tiles to the right
-        if(tileX < boardX and tileY > 0):
+        if(tileX < boardX-1 and tileY > 0):
             self.neighbouringTiles.append(self.farmBoard.grid[tileX +1, tileY -1])
-        if(tileX < boardX):
+        if(tileX < boardX-1):
             self.neighbouringTiles.append(self.farmBoard.grid[tileX +1, tileY])
-        if(tileX < boardX and tileY < boardY):
+        if(tileX < boardX-1 and tileY < boardY-1):
             self.neighbouringTiles.append(self.farmBoard.grid[tileX +1, tileY +1])
 
     def gadgetShutdown(self):
