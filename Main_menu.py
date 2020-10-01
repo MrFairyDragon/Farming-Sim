2f5750e212fb5442e30e4760d37f3127fe8d7ac2
AlexSkoett <asha17@student.aau.dk>
2020-10-01 08:10:02 +0200

    Small changes

--- Main_menu.py
+++ Main_menu.py
@@ -1,5 +1,6 @@
 import pygame
 from main import main
+import sys
 
 
 def StartTheGame():
@@ -9,7 +10,8 @@ def StartTheGame():
 class Main_menu:
 
     def __init__(self):
-        self.__screen = pygame.display.set_mode((800, 600))
+        self.size = (800, 600)
+        self.__screen = pygame.display.set_mode(self.size)
         self.__isInMenu = True
         self.__isPaused = False
         self.Widget1 = pygame.image.load('main_menu_assets/Multiplayer_Buttons.png')
@@ -34,24 +36,25 @@ class Main_menu:
                         self.isRunning = False
 
     def main_menu(self):
+        pygame.init()
         if self.isRunning:
             while self.isRunning:
-                for event in pygame.event.get():
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        self.mx, self.my = pygame.mouse.get_pos()
-                        self.checkIfClicked()
                 self.__screen.blit(self.background, (0 + (1 * self.counter), 0))
+                self.__screen.blit(self.background, (-self.size[0] + (1 * self.counter), 0))
                 self.__screen.blit(self.Widget1, self.playCoordinates, (0, 142, self.Widget1Length, self.Widget1Height))
                 self.__screen.blit(self.text, (377, 287))
                 pygame.display.update()
                 if self.isTrue:
                     self.counter += 1
-                    if self.counter == 100:
-                        self.isTrue = False
-                if not self.isTrue:
-                    self.counter -= 1
-                    if self.counter == 0:
-                        self.isTrue = True
+                if self.counter == 800:
+                    self.counter = 0
+                for event in pygame.event.get():
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        self.mx, self.my = pygame.mouse.get_pos()
+                        self.checkIfClicked()
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
             StartTheGame()
 
     def SelectTheScreen(self):
